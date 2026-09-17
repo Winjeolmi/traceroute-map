7f721c856d759d6428b156b0e348236bb61c17cb
Winjeolmi <chltnals1215@gmail.com>
2026-09-17 15:12:03 -0500

    Added command line input for destination IP and max hops

--- build-packet.py
+++ build-packet.py
@@ -1,6 +1,13 @@
 from scapy.all import IP, ICMP, sr1
+import sys # build-packet.py traceroute <destination IP> <max_hops>
+import time
 
-max_hops = 20
+if len(sys.argv) != 4:
+    print("Please provide valid command line arguments")
+    exit(0)
+
+dst = sys.argv[2]
+max_hops = int(sys.argv[3])
 
 for i in range(1, max_hops+1):
     pkt = IP(dst = "8.8.8.8", ttl=i)/ICMP()
