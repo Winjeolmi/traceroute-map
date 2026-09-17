6b3f286d693830e241ccdd5b6ae78fef674cfe2a
Winjeolmi <chltnals1215@gmail.com>
2026-09-17 14:55:08 -0500

    Made simple traceroute program

--- build-packet.py
+++ build-packet.py
@@ -1,4 +1,15 @@
-from scapy.all import IP, ICMP
+from scapy.all import IP, ICMP, sr1
 
-pkt = IP(dst="8.8.8.8", ttl=1)/ICMP()
-pkt.show()
+max_hops = 20
+
+for i in range(1, max_hops+1):
+    pkt = IP(dst = "8.8.8.8", ttl=i)/ICMP()
+    result = sr1(pkt, timeout = 2, verbose=0)
+    
+    if result == None:
+        continue
+
+    print("Hop #%d: %s" % (i, result.src))
+
+    if result.type == 0:
+        break
